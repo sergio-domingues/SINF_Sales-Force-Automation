1168f4ed218b0add07f4bb7ed4ea7d363d2d695a
gtugablue <silva95gustavo@gmail.com>
2016-11-14 02:18:02 +0000

    Clientes

---
+++ PharmaCRM/PharmaCRM/Controllers/ClientesController.cs
@@ -0,0 +1,18 @@
+﻿using PharmaCRM.Lib_Primavera.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PharmaCRM.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        public IEnumerable<Cliente> GetCliente()
+        {
+            return PharmaCRM.Lib_Primavera.PriIntegration.ListaClientes();
+        }
+    }
+}
