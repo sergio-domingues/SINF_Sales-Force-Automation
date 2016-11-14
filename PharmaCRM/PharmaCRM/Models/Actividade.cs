1378f4f23cc93fe1b68b838231f0568c6a62f9c7
sergio.mieic <sergio.mieic@gmail.com>
2016-11-14 01:31:12 +0000

    Models

---
+++ PharmaCRM/PharmaCRM/Models/Actividade.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PharmaCRM.Models
+{
+    public class Actividade
+    {
+        //todo ENUM for attr type
+
+        public string name
+        {
+            get;
+            set;
+        }
+
+        public string type
+        {
+            get;
+            set;
+        }
+
+        public bool finished
+        {
+            get;
+            set;
+        }
+
+        public string sales_rep 
+        {
+            get;
+            set;
+        }
+
+        public string address
+        {
+            get;
+            set;
+        }
+    }
+}
