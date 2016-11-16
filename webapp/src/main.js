dd3d748ebd0cfa583b5269ab3e1f92e91b0c83ea
dimamo5 <diogomoura13@gmail.com>
2016-11-16 02:16:22 +0000

    Activity and 404

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -6,16 +6,22 @@ import CustomerList from './components/CustomerList.vue'
 import Dashboard from './components/Dashboard.vue'
 import Lead from './components/Lead.vue'
 import LeadList from './components/LeadList.vue'
+import Activity from './components/Activity.vue'
+import ActivityList from './components/ActivityList.vue'
+import NotFound from './components/NotFound.vue'
 
 
 Vue.use(VueRouter);
 
 const routes = [
+  { path: '/', component: Dashboard },
   { path: '/customers', component: CustomerList },
   { path: '/customers/:id', component: Customer },
-  { path: '/', component: Dashboard },
   { path: '/leads', component: LeadList },
-  { path: '/leads/:id', component: Lead }
+  { path: '/leads/:id', component: Lead },
+  { path: '/activities', component: ActivityList },
+  { path: '/activities/:id', component: Activity },
+  { path: '*', component: NotFound },
 ]
 
 //Add history mode in future
