bd8173bd511f643ec403e2b9fe58e5516004aab8
André Lago <andrelago1995@gmail.com>
2016-11-17 16:22:31 +0000

    Improved Encomendas

--- PharmaCRM/PharmaCRM/Lib_Primavera/PriIntegration.cs
+++ PharmaCRM/PharmaCRM/Lib_Primavera/PriIntegration.cs
@@ -739,8 +739,6 @@ namespace PharmaCRM.Lib_Primavera
             }
         }
 
-
-
         public static List<Model.DocVenda> Encomendas_List()
         {
 
@@ -791,9 +789,6 @@ namespace PharmaCRM.Lib_Primavera
             }
             return listdv;
         }
-
-
-
 
         public static Model.DocVenda Encomenda_Get(string numdoc)
         {
@@ -926,7 +921,7 @@ namespace PharmaCRM.Lib_Primavera
             if (PriEngine.InitializeCompany(PharmaCRM.Properties.Settings.Default.Company.Trim(), PharmaCRM.Properties.Settings.Default.User.Trim(), PharmaCRM.Properties.Settings.Default.Password.Trim()) == true)
             {
 
-                StdBELista objListCab = PriEngine.Engine.Consulta("SELECT NumDoc, Data, Entidade, TotalMerc, TotalIva, TotalDesc, DataVencimento"
+                StdBELista objListCab = PriEngine.Engine.Consulta("SELECT id, NumDoc, Data, Entidade, TotalMerc, TotalIva, TotalDesc, DataVencimento"
                     + " FROM CabecDoc WHERE TipoDoc='ECL' AND NumDoc='" + numDocumento + "'");
 
                 if (objListCab.NoFim())
@@ -949,7 +944,7 @@ namespace PharmaCRM.Lib_Primavera
             {
                 List<Lib_Primavera.Model.Encomenda> encs = new List<Model.Encomenda>();
 
-                StdBELista objListCab = PriEngine.Engine.Consulta("SELECT NumDoc, Data, Entidade, TotalMerc, TotalIva, TotalDesc, DataVencimento"
+                StdBELista objListCab = PriEngine.Engine.Consulta("SELECT TOP 400 id, NumDoc, Data, Entidade, TotalMerc, TotalIva, TotalDesc, DataVencimento"
                     + " FROM CabecDoc WHERE TipoDoc='ECL'");
 
                 while (!objListCab.NoFim())
@@ -973,7 +968,7 @@ namespace PharmaCRM.Lib_Primavera
             {
                 List<Lib_Primavera.Model.Encomenda> encs = new List<Model.Encomenda>();
 
-                StdBELista objListCab = PriEngine.Engine.Consulta("SELECT NumDoc, Data, Entidade, TotalMerc, TotalIva, TotalDesc, DataVencimento"
+                StdBELista objListCab = PriEngine.Engine.Consulta("SELECT id, NumDoc, Data, Entidade, TotalMerc, TotalIva, TotalDesc, DataVencimento"
                     + " FROM CabecDoc WHERE TipoDoc='ECL' AND Entidade='" + codCliente + "'");
 
                 while (!objListCab.NoFim())
@@ -993,6 +988,7 @@ namespace PharmaCRM.Lib_Primavera
         public static Lib_Primavera.Model.Encomenda parseEncomenda(StdBELista objListCab)
         {
             Model.Encomenda enc = new Model.Encomenda();
+            enc.id = objListCab.Valor("id");
             enc.NumeroDocumento = objListCab.Valor("NumDoc");
             enc.Data = objListCab.Valor("Data");
             enc.Entidade = objListCab.Valor("Entidade");
@@ -1000,6 +996,28 @@ namespace PharmaCRM.Lib_Primavera
             enc.TotalIva = objListCab.Valor("TotalIva");
             enc.TotalDesconto = objListCab.Valor("TotalDesc");
             enc.DataVencimento = objListCab.Valor("DataVencimento");
+
+            StdBELista objListLin = PriEngine.Engine.Consulta("SELECT idCabecDoc, Artigo, Descricao, Quantidade, Unidade, PrecUnit, Desconto1, TotalILiquido, PrecoLiquido "
+                + "FROM LinhasDoc where IdCabecDoc='" + enc.id + "' order By NumLinha");
+            enc.Linhas = new List<Model.LinhaEncomenda>();
+
+            while (!objListLin.NoFim())
+            {
+                Model.LinhaEncomenda linha = new Model.LinhaEncomenda();
+                linha.IdCabecDoc = objListLin.Valor("idCabecDoc");
+                linha.CodArtigo = objListLin.Valor("Artigo");
+                linha.DescArtigo = objListLin.Valor("Descricao");
+                linha.Quantidade = objListLin.Valor("Quantidade");
+                linha.Unidade = objListLin.Valor("Unidade");
+                linha.Desconto = objListLin.Valor("Desconto1");
+                linha.PrecoUnitario = objListLin.Valor("PrecUnit");
+                linha.TotalILiquido = objListLin.Valor("TotalILiquido");
+                linha.TotalLiquido = objListLin.Valor("PrecoLiquido");
+                enc.Linhas.Add(linha);
+
+                objListLin.Seguinte();
+            }
+
             return enc;
         }
 
