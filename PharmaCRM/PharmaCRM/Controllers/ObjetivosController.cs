2244ced14a33c6a5d59470f1f15434a2d579aebf
André Lago <andrelago1995@gmail.com>
2016-11-21 10:39:46 +0000

    Started Goal model and controller

---
+++ PharmaCRM/PharmaCRM/Controllers/ObjetivosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PharmaCRM.Controllers
+{
+    public class ObjetivosController : ApiController
+    {
+        // GET: api/Objetivos
+        public IEnumerable<string> Get()
+        {
+            // TODO
+            throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+        }
+
+        // GET: api/Objetivos/5
+        public string Get(int id)
+        {
+            // TODO
+            throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+        }
+
+        // POST: api/Objetivos
+        public void Post([FromBody]string value)
+        {
+            // TODO
+            throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+        }
+
+        // PUT: api/Objetivos/5
+        public void Put(int id, [FromBody]string value)
+        {
+            // TODO
+            throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+        }
+
+        // DELETE: api/Objetivos/5
+        public void Delete(int id)
+        {
+            // TODO
+            throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+        }
+    }
+}
---
+++ PharmaCRM/PharmaCRM/Lib_Primavera/Model/Objetivo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmaCRM.Lib_Primavera.Model
+{
+    public class Objetivo
+    {
+        public string CodigoVendedor
+        {
+            get;
+            set;
+        }
+
+        public double Valor
+        {
+            get;
+            set;
+        }
+    }
+}
