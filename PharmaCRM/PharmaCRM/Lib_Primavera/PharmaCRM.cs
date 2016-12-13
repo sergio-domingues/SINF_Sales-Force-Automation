de5a8722189136f638b359b19543dbe5ddb817a1
André Lago <andrelago1995@gmail.com>
2016-12-13 17:08:09 +0000

    SQL file

---
+++ PharmaCRM/PharmaCRM/Lib_Primavera/PharmaCRM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace PharmaCRM.Lib_Primavera
+{
+    public class PharmaCRM
+    {
+        public static bool isVendedorPassword(string vendedor, string password)
+        {
+            string connectionString = "Data Source=USER-PC\\Primavera;Initial Catalog=PHARMACRM;User ID=sa;Password=Feup2014";
+            SqlConnection cnn = new SqlConnection(connectionString);
+            cnn.Open();
+
+            string queryString = "SELECT Vendedor FROM Authentication WHERE Vendedor = @vendedor AND Password = @password";
+
+            SqlCommand command = new SqlCommand(queryString, cnn);
+            command.Parameters.AddWithValue("@vendedor", vendedor);
+            command.Parameters.AddWithValue("@password", password);
+
+            bool result = false;
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                result = true;
+            }
+
+            reader.Close();
+            cnn.Close();
+
+            return result;
+        }
+    }
+}
---
+++ pharmacrm.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [PHARMACRM].[dbo].[Authentication] (
+   Vendedor nchar(3) NOT NULL PRIMARY KEY,
+   Password nchar(20) NOT NULL
+)
