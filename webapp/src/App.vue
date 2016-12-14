4a776c965c53bf13da77e35e2c39c0aa0f4f15ae
dimamo5 <diogomoura13@gmail.com>
2016-12-14 00:58:50 +0000

    Login done but not tested

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <navbar></navbar>
-    <sidebar></sidebar>
-    <router-view></router-view>
+    <navbar v-show="auth"></navbar>
+    <sidebar v-show="auth" ></sidebar>
+    <router-view v-show="auth"></router-view>
+
+    <login v-on:loggedIn="loggedIn" v-show="!auth"></login>
   </div>
 </template>
 <script>
@@ -10,13 +12,23 @@
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Breadcrumb from './components/Breadcrumb'
+import Login from './components/Login'
 
 export default {
   name: 'app',
+  data:function(){
+    return {auth:this.$root.auth}
+  },
   components: {
     Navbar,
     Sidebar,
-    Breadcrumb
+    Breadcrumb,
+    Login
+  },
+  methods:{
+    loggedIn:function(){
+      this.auth=true;
+    }
   }
 }
 </script>
---
+++ webapp/src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+
+      <form v-on:submit.prevent="requestLogin" class="form-signin">
+        <h2 class="form-signin-heading">Please Login</h2>
+        <label for="inputEmail" class="sr-only">Email</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+      </form>
+
+    </div> <!-- /container -->
+</template>
+
+<script>
+import config from '../assets/config.json'
+
+export default {
+  name: 'Login',
+  data () {
+    return {failed:false}     //todo
+  },
+  methods:{
+      requestLogin:function(){
+        this.$http.post(config.host+'/api/authentication')
+          .then((response)=>{
+              if(response ==='___'){  //admin mode
+                  this.$root.auth=true;
+                  this.$root.adminMode=true;
+                  this.$emit('loggedIn');
+              }else if(!isNaN(response)){   //sales rep
+                this.$root.auth=true;
+                this.$root.vendedor.id=response;
+                this.$emit('loggedIn');
+              }else{
+                alert('Email/Password não estão correctas!');
+              }
+          })
+      }
+  }
+
+}
+</script>
